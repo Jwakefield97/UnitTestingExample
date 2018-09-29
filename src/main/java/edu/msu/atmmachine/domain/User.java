dded2c7f25cfb8b53ad098827bc7cddb723a2095
Wakefield <jsw5227@live.missouristate.edu>
2018-09-29 18:10:24 -0500

    add user class

--- src/main/java/edu/msu/atmmachine/domain/User.java
+++ src/main/java/edu/msu/atmmachine/domain/User.java
@@ -1,5 +1,52 @@
 package edu.msu.atmmachine.domain;
 
 public class User {
+	private String username;
+	private String firstName;
+	private String lastName; 
+	private double balance; 
+	
+	User() {
+		
+	}
+	
+	User(String username, String firstName, String lastName, double balance) {
+		super();
+		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.balance = balance;
+	}
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
 }
