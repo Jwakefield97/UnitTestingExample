dde57f97148dae00d76d2f2b323272db194aced7
Wakefield <jsw5227@live.missouristate.edu>
2018-09-29 18:23:49 -0500

    add sql statements to dao object

--- src/main/java/edu/msu/atmmachine/dao/AtmDao.java
+++ src/main/java/edu/msu/atmmachine/dao/AtmDao.java
@@ -11,31 +11,31 @@ public class AtmDao {
 		this.connection = connection;
 	}
 	
-	private static final String CREATE_USER = "";
+	private static final String CREATE_USER = "INSERT INTO user VALUES (?,?,?,?)";
 	
 	public void createUser(String username, String firstName, String lastName) {
 		
 	}
 	
-	private static final String GET_USER = "";
+	private static final String GET_USER = "SELECT * FROM user WHERE username = ?";
 	
 	public User getUser(String username) {
 		return null;
 	}
 	
-	private static final String IS_USER_EXISTS = "";
+	private static final String IS_USER_EXISTS = "SELECT username FROM user WHERE username = ?";
 	
 	public boolean isUserExists(String username) {
 		return false;
 	}
 	
-	private static final String UPDATE_BALANCE = "";
+	private static final String UPDATE_BALANCE = "UPDATE user SET balance = ? WHERE username = ?";
 	
 	public void updateBalance(String username, double newBalance) {
 		
 	}
 	
-	private static final String GET_BALANCE = "";
+	private static final String GET_BALANCE = "SELECT balance FROM user WHERE username = ?";
 	
 	public double getBalance(String username) {
 		return 0;
